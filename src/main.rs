47005a1e794f6e4997dd5272ea5e6958a7c636bf
Alvaro Daniel Morales <daniel85mg@gmail.com>
2022-08-20 13:12:44 -0300

    Clap and already rename the package cuz i'm an insecure dev

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   #[clap(short, long, value_parser)]
+   filename: String,
+   
+   #[clap(short, long, value_parser)]
+   column: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+   let args = Args::parse();
+
+   println!("We are going to open: {}, and look for {}", args.filename, args.column);
 }

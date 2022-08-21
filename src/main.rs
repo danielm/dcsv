48df10a9e8a992f66c3e4436fd881fc2dd8a2b23
Alvaro Daniel Morales <daniel85mg@gmail.com>
2022-08-21 20:55:30 -0300

    initial csv

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 use clap::Parser;
+use csv::ReaderBuilder;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
    #[clap(short, long, value_parser)]
-   filename: String,
+   path: String,
    
    #[clap(short, long, value_parser)]
    column: String,
 }
 
 fn main() {
-   let args = Args::parse();
+    let args = Args::parse();
 
-   println!("We are going to open: {}, and look for {}", args.filename, args.column);
+    // println!("We are going to open: {}, and look for {}", args.filename, args.column);
+
+    match ReaderBuilder::new()
+        .delimiter(b',')
+        // .has_headers(false)
+        .from_path(args.path) {
+            Ok(mut reader) => {
+                if let Ok(headers) = reader.headers() {
+                    // println!("{:?}", headers);
+                    if let Some(colIndex) = headers.iter().position(|r| r == args.column) {
+                        // println!("Found header at index: {}", colIndex);
+                        // println!("{:?}", reader);
+
+                        for Ok(result) in reader.records() {
+                            if let Some(value) = result.get(colIndex) {
+                                println!("{:?}: {}", result.position(), value);
+                            }
+                        }
+                    } else {
+                        println!("Cannot find the requested column: '{}'", args.column);
+                    }
+                } else {
+                    println!("Cannot read headers");
+                }
+            },
+            Err(error) => println!("Cannot read CSV: {}", error),
+    }
+    
 }
+
+// fn print_column(reader: Reader, column: i32) 
+// {
+//     println!("we here: {}", column);
+// }
